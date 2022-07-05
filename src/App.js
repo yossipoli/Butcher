9c2c5ea416a2c3b3b781064f788ae6ea2a12c934
yossipoli <yossipoli@gmail.com>
2022-07-05 13:30:59 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './Nav'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
+        <Footer/>
       </header>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+  return (
+    <footer fixed='bottom' className="alert alert-danger" role="alert">
+        © 🐮Butcher, everything is fresh and high quality ©
+    </footer>
+  )
+}
+
+export default Footer
