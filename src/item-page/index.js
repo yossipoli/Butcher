98b3fbf6b559395de00f584760965e20b34302c5
yossipoli <yossipoli@gmail.com>
2022-07-30 18:29:35 +0300

    change color and nav buttons

--- src/item-page/index.js
+++ src/item-page/index.js
@@ -4,7 +4,7 @@ import Description from "./description";
 // import FullDescription from './full-description'
 import AddToCartButtons from "../AddToCartButtons";
 import "./index.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, Redirect } from "react-router-dom";
 import {Products, Images} from "./../DAL/api"
 import {useState , useEffect} from 'react'
 
@@ -14,26 +14,34 @@ function ItemPage() {
   const [product, setProduct] = useState()
   const {product_id} = useParams()
 
-  // useEffect(() => {
+  useEffect(() => {
     async function getData(){
       setImages(await Images.getAllImagesByProductId(parseInt(product_id)))
       setProduct(await Products.getObjectById(parseInt(product_id)))
     }
     getData()
-  // }, [])
+  }, [])
 
+console.log('product: ', product);
   return (
     <div className="page">
       { product ?
-        <div className="row">
-        <div className="col productPictures">
-          <Gallery images={images}/>
-        </div>
-        <div className="col">
-          <Description {...product}/>
-          <AddToCartButtons />
-        </div>
-      </div>
+        Object.keys(product).length ?
+          <div className="row">
+          <div className="col productPictures">
+            <Gallery images={images}/>
+          </div>
+          <div className="col">
+            <Description {...product}/>
+            <AddToCartButtons />
+          </div>
+        </div> 
+        : <Link to='/'>
+            <div className="notFound row">
+              You are looking for unknown product <br/>
+              click here to return Home Page
+            </div>
+          </Link>
       : "LOADING..."
       }
       <div className="row backHome">
