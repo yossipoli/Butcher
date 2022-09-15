17243b2f7ee5eb313d0a61fdcd69525561f2ec83
yossipoli <yossipoli@gmail.com>
2022-09-15 15:50:36 +0300

    change cart items to 1 fetch instead per item

--- src/Components/cart/CartItem.js
+++ src/Components/cart/CartItem.js
@@ -4,58 +4,73 @@ import AddToCartButtons from "./../item-page/AddToCartButtons";
 import "./CartItem.css";
 import api from "../../DAL/api";
 
-function CartItem( {bill, setBill, ...props}) {
-    const [productDetails, setProductDetails] = useState(null);
-    const [productImage, setProductImage] = useState(null);
-    
-    const [total, setTotal] = useState(0);
+function CartItem({
+    id,
+    name,
+    price,
+    description,
+    product_amount,
+    stock,
+    setTotalBill,
+    totalBill,
+}) {
+    // const [productDetails, setProductDetails] = useState(null);
+    const [productImage, setProductImage] = useState();
 
+    let [amount, setAmount] = useState(product_amount);
 
-    const [amount, setAmount] = useState(props.amount);
+    let [total, setTotal] = useState(amount * price);
 
     function handleAmountChange(e) {
-      setAmount(e.target.value)
-      // setTotal(productDetails.price * amount)
+        amount = e.target.value;
+        if (amount > stock) {
+            alert(`The maximum amount in stock is ${stock}`);
+            e.target.value = stock;
+            amount = stock;
+            setAmount(stock);
+        } else {
+            setAmount(amount);
+        }
+        let newBill = totalBill - total;
+        total = price * amount;
+        newBill += total;
+        setTotalBill(newBill);
+        setTotal(total);
     }
 
     useEffect(() => {
         async function getItemData() {
-            setProductDetails({ ...await api.getProduct(props.product_id) });
-            setProductImage({ ...await api.getImage(props.product_id) });
-            const newTotal = productDetails.price * amount
-            setTotal(newTotal)
-            setBill(bill + total)
+            setProductImage({ ...(await api.getImage(id)) });
         }
         getItemData();
     }, []);
 
     return (
-        <div className={`row cartItem`}>
+        <div className={`cartItem row`}>
             <div className="col cartPic">
                 <img src={productImage?.src} alt="meat" />
             </div>
             <div className={`col description`}>
                 <div className="name col">
-                    <h3>{productDetails?.name}</h3>
+                    <h3>{name}</h3>
                 </div>
-                {productDetails?.description}
+                {description}
                 <div className="price row">
-                    <AddToCartButtons amount={amount} hideAddBtn={true} onChange={handleAmountChange}/>
-                    <div className="in-col">
-                        <div>
-                            <h6>Price: {productDetails?.price}₪ for 1kg.</h6>
-                        </div>
-                        <div>
-                            <h6>
-                                total: {total}₪
-                            </h6>
-                        </div>
+                    <AddToCartButtons
+                        amount={amount}
+                        hideAddBtn={true}
+                        onChange={handleAmountChange}
+                    />
+                    <div>
+                        <h6>Price: {price}₪ for 1kg.</h6>
+                    </div>
+                    <div>
+                        <h6>Total: {total}₪</h6>
+                    </div>
+                    <div className="col-1 buttons">
+                        <Button variant="outline-danger">Remove</Button>{" "}
                     </div>
                 </div>
-            </div>
-            
-            <div className="col-1 buttons">
-                <Button variant="outline-danger">Remove</Button>{" "}
             </div>
         </div>
     );
--- src/Components/personal/Personal.js
+++ src/Components/personal/Personal.js
@@ -3,12 +3,13 @@ import ListGroup from "react-bootstrap/ListGroup";
 import "./Personal.css";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import api from "../../DAL/api";
+import PagesComponent from "./PagesComponent";
 
 function Personal() {
     const navigate = useNavigate();
     useEffect(() => {
         async function checkPermission() {
-            !(await api.checkCookie()) && navigate('/login');
+            !(await api.checkCookie()) && navigate("/login");
         }
         checkPermission();
     }, []);
@@ -16,45 +17,10 @@ function Personal() {
         <div>
             <h1>Personal Page</h1>
 
-            <div className="row start-position">
-                <div className="col-3 leftCol">
-                    <ListGroup as="ul">
-                        <Link to="personal-details">
-                            {" "}
-                            <ListGroup.Item
-                                as="li"
-                                className="personalOption"
-                                id="personalDetails"
-                            >
-                                {" "}
-                                Personal details{" "}
-                            </ListGroup.Item>{" "}
-                        </Link>
-                        <Link to="cart">
-                            {" "}
-                            <ListGroup.Item
-                                as="li"
-                                className="personalOption"
-                                id="cart"
-                            >
-                                {" "}
-                                My Cart{" "}
-                            </ListGroup.Item>{" "}
-                        </Link>
-                        <Link to="history">
-                            <ListGroup.Item
-                                as="li"
-                                className="personalOption"
-                                id="history"
-                            >
-                                {" "}
-                                Orders history{" "}
-                            </ListGroup.Item>{" "}
-                        </Link>
-                    </ListGroup>
-                </div>
+            <div className="col start-position">
+                <PagesComponent />
 
-                <div className="col-9">
+                <div className="row">
                     <div className="row">
                         <Outlet />
                     </div>
