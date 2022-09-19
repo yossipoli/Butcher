eff65b292a4356687d2a73f9d8be06f68048ab20
yossipoli <yossipoli@gmail.com>
2022-09-19 16:01:37 +0300

    register in data base

--- src/Components/forms/Register.js
+++ src/Components/forms/Register.js
@@ -1,117 +1,135 @@
-import React from 'react'
-import './Register.css'
-import Form from './Form'
+import React from "react";
+import "./Register.css";
+import Form from "./Form";
+import api from "../../DAL/api";
+import { useNavigate } from "react-router-dom";
 
-function Register({firstName="", lastName="",email="",password=""}) {
-  const formInputs = {
-    firstName: {
-      id: 1,
-      name: "firstName",
-      label: "First name",
-      placeholder: "Please enter your first name",
-      value: firstName,
-      type: "text",
-      validations: {
-        required: true,
-        minLength: 2,
-      },
-      errors: [],
-    },
-    lastName: {
-      id: 2,
-      name: "lastName",
-      label: "Last name",
-      placeholder: "Please enter your last name",
-      value: lastName,
-      type: "text",
-      validations: {
-        required: true,
-        minLength: 2,
-      },
-      errors: [],
-    },
-    email: {
-      id: 3,
-      name: "email",
-      label: "Email",
-      placeholder: "Please enter your email",
-      value: email,
-      type: "email",
-      validations: {
-        required: true,
-        pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-      },
-      errors: [],
-    },
-    confirmEmail: {
-      id: 4,
-      name: "confirmEmail",
-      label: "Confirm Email",
-      placeholder: "Please re-enter your email for validation",
-      value: email,
-      type: "email",
-      validations: {
-        required: true,
-        selfsame: "email"
-      },
-      errors: [],
-    },
-    password: {
-      id: 5,
-      name: "password",
-      label: "Password",
-      placeholder: "Please enter a password",
-      value: password,
-      type: "password",
-      validations: {
-        required: true,
-        minLength: 8,
-      },
-      errors: [],
-    },
-  
-    confirmPassword: {
-      id: 6,
-      name: "confirmPassword",
-      label: "Confirm password",
-      placeholder: "Please re-enter your password for validation",
-      value: password,
-      type: "password",
-      validations: {
-        required: true,
-        selfsame: "password"
-      },
-      errors: [],
-    },
-  
-    // agree: {
-    //   id: 7,
-    //   name: "agree",
-    //   label: "Agree",
-    //   value: false,
-    //   type: "checkbox",
-    //   validations: {
-    //     required: true,
-    //   },
-    //   errors: [],
-    // },
-  };
+function Register({
+    firstName = "",
+    lastName = "",
+    email = "",
+    password = "",
+}) {
+    const navigate = useNavigate();
 
-  const onSubmitFunc = (values) => {
-    alert("Welcome")
-  };
+    const formInputs = {
+        firstName: {
+            id: 1,
+            name: "firstName",
+            label: "First name",
+            placeholder: "Please enter your first name",
+            value: firstName,
+            type: "text",
+            validations: {
+                required: true,
+                minLength: 2,
+            },
+            errors: [],
+        },
+        lastName: {
+            id: 2,
+            name: "lastName",
+            label: "Last name",
+            placeholder: "Please enter your last name",
+            value: lastName,
+            type: "text",
+            validations: {
+                required: true,
+                minLength: 2,
+            },
+            errors: [],
+        },
+        email: {
+            id: 3,
+            name: "email",
+            label: "Email",
+            placeholder: "Please enter your email",
+            value: email,
+            type: "email",
+            validations: {
+                required: true,
+                pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            },
+            errors: [],
+        },
+        confirmEmail: {
+            id: 4,
+            name: "confirmEmail",
+            label: "Confirm Email",
+            placeholder: "Please re-enter your email for validation",
+            value: email,
+            type: "email",
+            validations: {
+                required: true,
+                selfsame: "email",
+            },
+            errors: [],
+        },
+        password: {
+            id: 5,
+            name: "password",
+            label: "Password",
+            placeholder: "Please enter a password",
+            value: password,
+            type: "password",
+            validations: {
+                required: true,
+                minLength: 8,
+            },
+            errors: [],
+        },
 
-  return (
-    <div>
-          <Form
-          title= "Register"
-          subTitle = "Already registered? click here!"
-          formInputs = {formInputs}
-          onSubmitFunc = {onSubmitFunc}
-          link="/login"
-          />
-    </div>
-  )
+        confirmPassword: {
+            id: 6,
+            name: "confirmPassword",
+            label: "Confirm password",
+            placeholder: "Please re-enter your password for validation",
+            value: password,
+            type: "password",
+            validations: {
+                required: true,
+                selfsame: "password",
+            },
+            errors: [],
+        },
+
+        // agree: {
+        //   id: 7,
+        //   name: "agree",
+        //   label: "Agree",
+        //   value: false,
+        //   type: "checkbox",
+        //   validations: {
+        //     required: true,
+        //   },
+        //   errors: [],
+        // },
+    };
+
+    const onSubmitFunc = (values) => {
+        const registrationDetails = {
+            first_name: values.firstName,
+            last_name: values.lastName,
+            email: values.email,
+            password: values.password,
+        };
+        const isNewEmail = api.register(registrationDetails);
+        isNewEmail
+            ? navigate("login")
+            : alert("This email already registered and in use");
+    };
+
+    return (
+        <div>
+            <Form
+                title="Register"
+                subTitle="Already registered? click here!"
+                formInputs={formInputs}
+                onSubmitFunc={onSubmitFunc}
+                link="/login"
+            />
+        </div>
+    );
 }
 
-export default Register
+export default Register;
--- src/DAL/api.js
+++ src/DAL/api.js
@@ -1,10 +1,3 @@
-import users_data from './../Data/Users.json'
-import carts_data from './../Data/carts.json'
-import categories_data from './../Data/categories.json'
-import images_data from './../Data/images.json'
-import order_details_data from './../Data/order_details.json'
-import orders_data from './../Data/orders.json'
-import products_data from './../Data/products.json'
 
 //POST requests options
 function getRequestOption(obj) {
@@ -18,14 +11,15 @@ function getRequestOption(obj) {
 }
 
 export default class api {
-  // constructor(data) {
-  //   this._data = data;
-  // }
+
+  static async register(values) {
+    const requestOptions = getRequestOption(values)
+    return await fetch('http://localhost:4000/customers/register', requestOptions).then(res=>res.json())
+  }
 
   static async login(values) {
     const requestOptions = getRequestOption(values)
-    return await fetch('http://localhost:4000/customers', requestOptions)
-      .then(res=>res.json())
+    return await fetch('http://localhost:4000/customers', requestOptions) .then(res=>res.json())
   }
 
   static async getAllCategories() {
@@ -77,91 +71,6 @@ export default class api {
     const requestOptions = getRequestOption(product_id)
     return await fetch('http://localhost:4000/cart/remove', requestOptions).then(res=> res.json())
   }
-
-  // get data() {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve(this._data);
-  //     }, 2000);
-  //   });
-  // }
-
-  // getCategoryIdByName(name){
-  //   return new Promise((resolve, reject)=>{
-  //     setTimeout(() => {
-  //       const category = this._data.find(category=>category.name.toLowerCase()===name.toLowerCase())
-  //       resolve(category.id || null)
-  //     }, 500);
-  //   })
-  // }
-
-  // set data(newData) {
-
-  //     setTimeout(() => {
-  //       this._data = newData;
-  //       // sessionStorage.setItem("data", JSON.stringify(this._data));
-  //     }, 2000);
-
-  // }
-
-  // getObjectById(id) {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve(this._data.find(obj=>obj.id === id) || {});
-  //     }, 2000);
-  //   });
-  // }
-
-  // getImageByProductId(id) {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve(this._data.find(obj=>obj.product_id === id));
-  //     }, 2000);
-  //   });
-  // }
-
-  // getAllImagesByProductId(id) {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve(this._data.filter(obj=>obj.product_id === id));
-  //     }, 2000);
-  //   });
-  // }
-
-  // addObject(newItem) {
-
-  //     setTimeout(() => {
-  //       this._data.push(newItem);
-  //       // localStorage.setItem("data", JSON.stringify(this._data));
-  //     }, 2000);
-
-  // }
-
-  // setObj(id, newItemData) {
-    
-  //   setTimeout(() => {
-  //     this._data[this._data.indexOf(obj=>obj.id === id)]=newItemData
-  //       // localStorage.setItem("data", JSON.stringify(this._data));
-  //     }, 2000);
-
-  // }
-
-  // removeItem(id) {
-      
-  //     setTimeout(() => {
-  //       this._data = this._data.filter(obj=>obj.id!==id)
-  //       // localStorage.setItem("data", JSON.stringify(this._data));
-  //     }, 2000);
-
-  // }
 }
 
-// const ApiProductsData = JSON.parse(sessionStorage.getItem("data")) || ProductsData;
-// export const Users = new api(users_data);
-// export const Carts = new api(carts_data);
-// export const Categories = new api(categories_data);
-// export const Images = new api(images_data);
-// export const Order_Details = new api(order_details_data);
-// export const Orders = new api(orders_data);
-// export const Products = new api(products_data);
 
